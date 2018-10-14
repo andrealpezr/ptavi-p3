cad94142637f43b3fc8c70dadf23ad46073f1fda
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2018-10-14 12:15:11 +0200

    Corregido con Pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,30 +5,30 @@ Created on Mon Oct  8 20:54:26 2018
 
 @author: andrea
 """
-import sys
+
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
 class SmallSMILHandler(ContentHandler):
-    def __init__(self):  # método para inicializar atributos
-        self.lista = []  # lista vacía que se rellenará
+    def __init__(self):  # Método para inicializar atributos
+        self.lista = []  # Lista vacía que se rellenará
+        # Creo un diccionario
         self.dicc = {'root-layout': ['width', 'height', 'background-color'],
-            'region': ['id', 'top', 'bottom', 'left', 'right'],                       
-            'img': ['src', 'region', 'begin', 'dur'], 
-            'audio': ['src', 'begin', 'dur'], 'textstream': ['src', 'region']}  
-         # creo un diccionario
+                     'region': ['id', 'top', 'bottom', 'left', 'right'],
+                     'img': ['src', 'region', 'begin', 'dur'],
+                     'audio': ['src', 'begin', 'dur'],
+                     'textstream': ['src', 'region']}
 
-    def startElement(self, name, attrs):
+    def startElement(self, name, attrs):  # Método cuando se abre etiqueta
         dict = {}
         if name in self.dicc:
-            dict['name'] = name
-            for atributo in self.dicc[name]:
-                dict[atributo] = attrs.get(atributo, "")
-            self.lista.append(dict)
+            for atrib in self.dicc[name]:  # Itera todos los elementos
+                dict[atrib] = attrs.get(atrib, "")
+            self.lista.append([name, dict])  # Añade al final de la lista
 
     def get_tags(self):
-        return(self.lista)
+        return self.lista  # Devuelve una lista con las etiquetas encontradas
 
 
 if __name__ == "__main__":
