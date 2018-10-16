684beb50d7015c8ab75a3b0b7e68f561af242063
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2018-10-16 23:52:19 +0200

    Falta PEP8

--- karaoke.py
+++ karaoke.py
@@ -14,42 +14,44 @@ from smallsmilhandler import SmallSMILHandler
 
 class KaraokeLocal(SmallSMILHandler):
 
-    def __init__(self):
+    def __init__(self,fichero):
         # Inicializo y construyo la lista
         parser = make_parser()  # Creo parser
         cHandler = SmallSMILHandler()  # Creo manejador
         parser.setContentHandler(cHandler)  # Le paso el parser al manejador
-        parser.parse(open(sys.argv[1]))
+        parser.parse(open(fichero))
         self.lista = cHandler.get_tags()
 
-    def __str__(self):  
-        # Recorro diccionario añadiendo un string y '\n'
-        string = ''
-        for dic in self.lista:
-            string = string + dic['name']
-            for atributo in dic:
-                if dic[atributo] != "" and atributo != 'name':
-                    string = string + "\t" + atributo + '='
-                    string = string + dic[atributo] + '"' + "\n"
-        return(string)
-
+    def __str__(self):
+        """ Método para crear la lista de etiquetas """
+        linea = " "
+        for elem in self.lista:
+            linea = linea + elem[0]
+            atributos = elem[1].items()
+            for nombre, valor in atributos:
+                if valor != '' :
+                    linea = linea + '\t' + nombre + '=' + '"' + valor + '"'
+            print(linea)
+        
+        
     def to_json(self, fich, fich_json=None):
         # Creamos un fichero en formato json
         fich_json = json.dumps(self.lista)
         if fich_json is None:
             fich_json = fich.split('.')[0] + '.json'
-        json.dump(self.lista, open(fich_json, 'w'))
+            json.dump(self.lista, open(fich_json, 'w'))
+               
 
     def do_local(self):
         # Recorre la lista y descarga recursos remotos
         for diccs in self.lista:
-            for atributos in diccs:
-                    if diccs[atributos][0:7] == "http://":
-                        atrib_Antes = diccs[atributos]
-                        atrib_Nuevo = diccs[atributos].split('/')[-1]
-                        urlretrieve(atrib_Antes, atrib_Nuevo)
-                        diccs[atributos] = atrib_Nuevo
-
+            atrib = diccs[1]
+            for atributos, posi in atrib.items():
+                    if atributos == "src" and posi[0:7] == "http://":
+                        atrib_Nuevo = posi.split('/')[-1]
+                        urlretrieve(posi, atrib_Nuevo)
+                        print("Descargando %s ..." % posi)
+                
 
 if __name__ == "__main__":
 
@@ -60,10 +62,12 @@ if __name__ == "__main__":
     except (ValueError, IndexError, FileNotFoundError):
         sys.exit("Usage: python3 karaoke.py file.smil")
  
-    obj = KaraokeLocal()
-    print(obj)
+    fichero = sys.argv[1]
+    fich_json = sys.argv[1].replace(".smil", ".json")
+    obj = KaraokeLocal(fichero)
+    obj.__init__(fichero)
     obj.__str__()
-    obj.to_json(sys.argv[1])
+    obj.to_json(fich_json)
     obj.do_local()
-    obj.to_json(sys.argv[1], 'local.json')
-    print(obj)
+    obj.to_json(fich_json, 'local.json')
+    obj.__str__()
